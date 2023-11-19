6fdf3f73a206f816bc6c9d77f9f371451561a4b5
+ <+>
2023-11-19 16:22:08 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers=[num for num in num_list if num % 2 == 0]
+    return even_numbers
+    
+numbers_even=return_evens([0, 1, 3, 5, 7, 8, 9])
+print (numbers_even)
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_with_exclamation=[word + "!" for word in sentence_list ]
+    return sentence_with_exclamation
+sentence=make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+print (sentence)
